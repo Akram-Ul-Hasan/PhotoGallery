70336d7a67d59e365a5d31c65e34da5fc324e226
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-09-07 13:55:51 +0600

    refactor(model): rename PhotoModel.swift to Photo.swift

---
+++ PhotoGallery/core/Models/Photo.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoModel.swift
+//  PhotoGallery
+//
+//  Created by Akram Ul Hasan on 9/7/25.
+//
+
+import Foundation
+
+struct Photo: Identifiable, Decodable {
+    let id: String
+    let author: String
+    let width: Int
+    let height: Int
+    let url: String
+    let downloadURL: String
+
+    enum codingKeys: String, CodingKey {
+        case id
+        case author
+        case width
+        case height
+        case url
+        case downloadURL = "download_url"
+    }
+}
