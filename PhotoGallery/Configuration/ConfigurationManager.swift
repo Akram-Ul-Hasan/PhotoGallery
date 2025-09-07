7324f8b45ee78f9bbe57e5d590c8114d47c2d1ef
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-09-07 21:51:12 +0600

    chore(config): add multiple build configurations (dev/prod)

---
+++ PhotoGallery/Configuration/ConfigurationManager.swift
@@ -0,0 +1,56 @@
+//
+//  ConfigurationManager.swift
+//  PhotoGallery
+//
+//  Created by Akram Ul Hasan on 9/7/25.
+//
+
+import Foundation
+
+private enum BuildConfiguration {
+    enum Error: Swift.Error {
+        case missingKey, invalidValue
+    }
+    
+    static func value<T>(for key: String) throws -> T where T: LosslessStringConvertible {
+        guard let object = Bundle.main.object(forInfoDictionaryKey: key) else {
+            throw Error.missingKey
+        }
+        
+        switch object {
+        case let string as String:
+            guard let value = T(string) else {
+                fallthrough
+            }
+            return value
+            
+        default:
+            throw Error.invalidValue
+        }
+    }
+}
+
+enum API {
+    static var baseURL: String {
+        do {
+            return try BuildConfiguration.value(for: "BASE_URL")
+        } catch {
+            fatalError(error.localizedDescription)
+        }
+    }
+}
+
+enum ConfigurationManager {
+    enum Environment {
+        case dev
+        case prod
+    }
+    
+    static var environment: Environment {
+        #if DEV
+        return .dev
+        #elseif PROD
+        return .prod
+        #endif
+    }
+}
