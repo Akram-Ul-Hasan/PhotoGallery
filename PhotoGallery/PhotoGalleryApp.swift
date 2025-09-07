c652b740f6ec09d553c0a0ba47bf5819b3c7b62e
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-09-07 12:48:14 +0600

    Initial Commit

---
+++ PhotoGallery/PhotoGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoGalleryApp.swift
+//  PhotoGallery
+//
+//  Created by Techetron Ventures Ltd on 9/7/25.
+//
+
+import SwiftUI
+
+@main
+struct PhotoGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
