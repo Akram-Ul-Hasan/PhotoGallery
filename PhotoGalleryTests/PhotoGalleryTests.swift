79c191500443ab3a1182b76cd58c5fdc652f1f1f
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-09-09 14:46:18 +0600

    test: add initial unit test target

---
+++ PhotoGalleryTests/PhotoGalleryTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoGalleryTests.swift
+//  PhotoGalleryTests
+//
+//  Created by Techetron Ventures Ltd on 9/9/25.
+//
+
+import Testing
+
+struct PhotoGalleryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
