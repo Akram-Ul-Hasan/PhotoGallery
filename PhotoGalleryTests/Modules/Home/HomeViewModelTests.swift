220ee0c424afb98ff6894d0a5599f59e0db85d8e
Md. Akram Ul Hasan <akramulhasan.dev@gmail.com>
2025-09-09 17:30:27 +0600

    "test: add success case for loadInitialPhotos in HomeViewModel"

--- PhotoGalleryTests/Modules/Home/HomeViewModelTests.swift
+++ PhotoGalleryTests/Modules/Home/HomeViewModelTests.swift
@@ -2,7 +2,7 @@
 //  HomeViewModelTests.swift
 //  PhotoGalleryTests
 //
-//  Created by Techetron Ventures Ltd on 9/9/25.
+//  Created by Akram Ul Hasan on 9/9/25.
 //
 
 import XCTest
@@ -13,15 +13,51 @@ final class HomeViewModelTests: XCTestCase {
 
     var cancellables: Set<AnyCancellable> = []
     var viewModel : HomeViewModel!
+    var mockNetWork: MockNetworkService!
+    var mockImageCache: MockImageCacheService!
+    
     
     override func setUp() {
         super.setUp()
         
+        mockNetWork = MockNetworkService()
+        mockImageCache = MockImageCacheService()
+        viewModel = HomeViewModel(networkService: mockNetWork, imageCacheService: mockImageCache)
     }
     
     override func tearDown() {
         cancellables.removeAll()
         viewModel = nil
+        mockNetWork = nil
+        mockImageCache = nil
         super.tearDown()
+    }
+    
+    func test_LoadInitialPhotos_Success() {
+        let mockPhotos: [Photo] = [
+            Photo(id: "1", author: "a1", width: 200, height: 200, url: "url1", downloadURL: "url1"),
+            Photo(id: "2", author: "a2", width: 200, height: 200, url: "url2", downloadURL: "url2"),
+        ]
+        
+        mockNetWork.photosToReturn = mockPhotos
+        let expectation = XCTestExpectation(description: "photo loading...")
+        
+        viewModel.$photos
+            .sink { photos in
+                if photos.count == 2 {
+                    expectation.fulfill()
+                }
+                
+            }
+            .store(in: &cancellables)
+        
+        viewModel.loadInitialPhotos()
+        
+        wait(for: [expectation], timeout: 1.0)
+        
+        XCTAssertEqual(viewModel.photos.count, 2)
+        XCTAssertEqual(viewModel.photos.first?.id, "1")
+        XCTAssertFalse(viewModel.isLoading)
+        
     }
 }
